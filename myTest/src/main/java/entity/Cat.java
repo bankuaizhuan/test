f02e2cfdf9c1d12df69cb25820408ecb0f9539c6
rtzhangjx <rtzhangjx@yonyou.com>
2021-05-20 10:28:26 +0800

    第2个类

---
+++ myTest/src/main/java/entity/Cat.java
@@ -0,0 +1,15 @@
+package entity;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: Jingxv Zhang
+ * 2021/5/20  10:27
+ * TODO
+ */
+@Data
+public class Cat {
+    private int id;
+    private short age;
+}
